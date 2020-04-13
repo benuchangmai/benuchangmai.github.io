5c071ed066224feebfc7e3f42d16f7327b18eeaa
benuChang <benu@ucsb.edu>
2020-04-13 16:17:12 -0700

    Modified scripts

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function setTabActive(){
-	document.querySelector(".tab.home").style.background = "rgba(128, 128, 128, 0.2)";
+function activeTab(tab){
+	var active_tab = document.querySelector(tab);
+	active_tab.style.background = "rgba(128, 128, 128, 0.2)";
 }
+
+var btt = document.getElementById('btt'),
+offset = 100,
+body = document.body,
+docElement = document.documentElement,
+scrollPos, docHeight;
+
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElement.clientHeight, docElement.scrollHeight, docElement.offsetHeight);
+if(docHeight!= 'undefined'){
+	offset = docHeight/4;
+}
+window.addEventListener("scroll", function(){
+	scrollPos = body.scrollTop || docElement.scrollTop;
+	btt.className = (scrollPos > offset)? 'visible' : '';
+});
+btt.addEventListener("click", function(){
+	event.preventDefault();
+	docElement.scrollTop = 0;
+	body.scrollTop = 0;
+});	
+
+
+
+
+
